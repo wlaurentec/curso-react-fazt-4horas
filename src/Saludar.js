991adfb90df4d107773eb5206203d28d52d4cb98
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-05 11:55:06 -0500

    creacion de componente con clase

---
+++ src/Saludar.js
@@ -0,0 +1,6 @@
+import { Component } from "react";
+export class Saludar extends Component {
+  render() {
+    return <h1>Hola</h1>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { TaskCard } from "./Task.js";
+import { Saludar } from "./Saludar.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<>
   <TaskCard ready={true}/>
+  <Saludar />
 </>);
