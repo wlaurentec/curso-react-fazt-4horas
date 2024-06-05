d71f0629dd07950e7b68711aed506c17dfda69aa
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-05 11:06:50 -0500

    primera fomra de agregar estilos

--- src/index.js
+++ src/index.js
@@ -1,37 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Greeting, UserCard } from "./Greeting";
-import Product, { Navbar } from "./Product";
-import Button from "./Button";
+import { TaskCard } from "./Task.js";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <>
-  <Button text="Click me"/>
-  <Button text="Play"/>
-  <Button text="Go to" name="Home"/>
-
-
-
-
-    {/* <UserCard
-      name="John"
-      amount={1000}
-      married={true}
-      points={[99, 22, 32]}
-      address={{ city: "New York", country: "USA" }}
-      greet={function () {
-        alert("Hello");
-      }}
-    />
-    <UserCard
-      name="Joe"
-      amount={2000}
-      married={false}
-      points={[99, 22, 32]}
-      address={{ city: "California", country: "USA" }}
-      
-    /> */}
-    </>
-);
+root.render(<>
+  <TaskCard />
+</>);
