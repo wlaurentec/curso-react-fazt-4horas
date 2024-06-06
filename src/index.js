37b94d99b70b1d9172d8d7f05797c4f910e23054
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-06 09:03:09 -0500

    capturando el mensaje ingreso en el input

--- src/index.js
+++ src/index.js
@@ -6,17 +6,14 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+  const [mensaje, setMensaje] = useState("");
   return (
     <div>
-      <h1>Counter: {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>Sumar</button>
-      <button onClick={() => setCounter(counter - 1)}>Restar</button>
-      <button onClick={() => setCounter(0)}>Reiniciar</button>
+      <input onChange={(e) => setMensaje(e.target.value)}/>
+      <button onClick={() => alert(mensaje)}>save</button>
     </div>
   );
 }
-console.log("Hello");
 root.render(
   <>
     <Counter />
