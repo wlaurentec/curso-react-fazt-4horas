c808e7874dbf99d827ddccc236ebe9b9a790376a
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-06 09:21:05 -0500

    useEfect con dependencia

--- src/index.js
+++ src/index.js
@@ -7,15 +7,19 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter() {
   const [mensaje, setMensaje] = useState("");
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     console.log("useEffect");
-  }, [ ]);
+  }, [ counter ]);
 
   return (
     <div>
       <input onChange={(e) => setMensaje(e.target.value)}/>
       <button onClick={() => alert(mensaje)}>save</button>
+      <hr />
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Sumar</button>
     </div>
   );
 }
