85d0da63b3e2c572af072498ea218fbad032da16
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-05 22:50:43 -0500

    trabajando con array

--- src/index.js
+++ src/index.js
@@ -3,12 +3,35 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Posts from "./Posts";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const user = [
+  {
+    id: 1,
+    name: "John",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Jane",
+    image: "https://robohash.org/user2",
+  },
+  {
+    id: 3,
+    name: "Marcos",
+    image: "https://robohash.org/user3",
+  },
+];
 
 root.render(
   <>
-    <Posts />
+    {user.map((user, i) => {
+      return (
+        <div key={i}>
+          <h1>{user.name}</h1>
+          <img src={user.image} alt="user" />
+        </div>
+      );
+    })}
   </>
 );
